7038b5dae8cf2f0407467e64d8a2733314be9c7d
ELsayedAhmed-p <ELsayedAhmed.p@gmail.com>
2023-11-03 08:03:48 +0200

    IR Photoelectric Encoder Sensor 4pin

--- mode_3/low_level/encoders/measuring_motor_speed/measuring_motor_speed.ino
+++ mode_3/low_level/encoders/measuring_motor_speed/measuring_motor_speed.ino
@@ -1,36 +1,41 @@
-/*******status: unfinished yet***********
+/*
+*************Status: Finished************
 *****************************************
 *
 *Name: ELsayed Ahmed Ibrahim
-*Last edit : 8:40 pm 2/11/2023
+*Last edit : 8:05 am 3/11/2023
 *
 *****************************************
+******************PINS*******************
+- VCC
+- GND
+- TLL Output "D0"
+- Analog Output "A0"
 */
 
-#define D0  2 //High Priority Intterupt
+#define A0  3 
 #define holes_wheel   //No. of holes in Encoder Wheel
-int encoder_counter = 0; //to count the number of raising pulses
+volatile byte pulses = 0; //to count the number of raising pulses
 int RPM = 0;
 unsigned long timer1 = 0;
 void RMS_Motor(); 
 void setup() {
-  pinMode(D0, INPUT);
+  pinMode(A0, INPUT);
   Serial.begin(9600);
-  attachIntterupt(digitalPinToIntterupt(D0) , RPM_Motor , RISING);
+  attachIntterupt(digitalPinToIntterupt(A0) , RPM_Motor , RISING);
   timer1 = millies();
     
 }
 
 void loop() {
-  if(millies()-timer1>=1000){
-    RPM = (encoder_counter / holes_wheel) * 60;
+  if(millies()-timer1>=(1000/holes_wheel)){
+    RPM = pulses * 60;
     timer = millies();
+    Serial.println("RPM = ");
+    Serial.print(RPM);
+    pulses = 0;
   }
-  
-  Serial.print("RPM = ");
-  Serial.print(RPM);
-  delay(1000);
 }
 void RMS_Motor(){
-  encoder_counter++;
+  pulses++;
 }
